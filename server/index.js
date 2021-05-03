acd07a150ee788bbc722976cb5fa1e980f27d5ed
blockmonkey <blockmonkey@naver.com>
2021-05-03 16:02:25 +0900

    DB Modeling & Remark

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
-
 //몽고 디비 연결
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, useUnifiedTopology: true,
