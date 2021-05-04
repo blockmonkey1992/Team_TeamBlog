1ba9ca05aac58af4542d02b3b9e2fcb88b63090d
hanpapa4 <hanpapa4@naver.com>
2021-05-04 20:53:31 +0900

    로그인창2

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { Modal, Button, Input, Space } from 'antd';
+import { Modal, Input, Space } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone} from '@ant-design/icons';
 import '../../Scss/Login.scss';
 
@@ -21,44 +21,47 @@ function Login() {
   };
     return (
       <div>
-        <Button type="primary" onClick={showModal}>
+        <div type="primary" onClick={showModal}>
           로그인
-        </Button>
-        <Modal title="Login to Ninja Coders" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} 
-        bodyStyle={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: '-30px',
-        }}
-        >
-        <Input
-         style={{
-          borderRadius:'5px',
-          marginTop: '30px',
-          // width: '475px'        
-        }}
-          type="email"
-          placeholder="E-mail"
-      
-        />
-        <Space direction="vertical">
-          <div>
-          <Input.Password
-            style={{
-              borderRadius:'5px',
-              marginTop: '30px',
-              width: '475px'        
-            }}
-              type="password"
-              placeholder="password"
-              iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            )}
+        </div>
+        
+        <Modal title="Login to Ninja Coders" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} >
+          <Input style={{
+            borderRadius:'5px',
+            marginTop: '30px',
+            width: '54vw'        
+          }}
+            type="email"
+            placeholder="E-mail"
           />
-          </div>
+          <Space direction="vertical">
+            <div>
+              <Input.Password
+                style={{
+                  borderRadius:'5px',
+                  marginTop: '30px',
+                  width: '54vw'        
+                }}
+                  type="password"
+                  placeholder="password"
+                  iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                )}
+              />
+            </div>
         </Space>
+        <div class="ant-modal-footer" onClick={handleOk}>
+          <button type="button" class="ant-btn ant-btn-primary" id="loginBtn">
+            <span>LOGIN</span>
+          </button>
+        </div>
+
+        <a href="/register"
+        className="RegisterIn">Register in</a>
+        
       </Modal>
+  
       </div>
+       
     )
 }
 
