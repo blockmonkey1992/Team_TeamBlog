fc3b1d94e2bcab08e263292ca007e38c35942533
kkevi <77606317+kkevi@users.noreply.github.com>
2021-05-11 16:10:45 +0900

    My Profile Page

--- client/src/Components/Profile/Sections/Comment.js
+++ client/src/Components/Profile/Sections/Comment.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import CommentComponent from './CommentComponent';
 
 function Comment() {
     return (
         <div className="myWrapper__container">
             <div className="myMy__title">작성 덧글</div>
+            <div className="myComment_contents">
+                <CommentComponent />
+            </div>
+           
         </div>
     )
 }
--- client/src/Components/Profile/Sections/Like.js
+++ client/src/Components/Profile/Sections/Like.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import LikeComponent from './LikeComponent';
+
 
 function Like() {
     return (
         <div className="myWrapper__container">
             <div className="myMy__title">좋아요 한 글 보기</div>
+            <div className="myLike__contents">
+                <LikeComponent />
+            </div>
         </div>
     )
 }
