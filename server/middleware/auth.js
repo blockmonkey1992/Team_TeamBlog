0bc18eeaf5720be2db329c39f463fcb76e2550fe
blockmonkey <blockmonkey@naver.com>
2021-05-17 17:54:07 +0900

    finish

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,6 @@ const { User } = require("../model/User");
 
 const auth = (req, res, next) => {
     const token = req.cookies.x_auth;
-    console.log("I AM AUTH!");
     //쿠키에서 토큰을 가져와 복호화해서 
     //-> 유저의 아이디값과 토큰을 비교해 유저확인
     //-> 유저가 존재하면 인증시켜주고, 없으면 인증시키면 인증안됨.
