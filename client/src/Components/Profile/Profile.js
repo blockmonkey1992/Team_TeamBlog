cc73b4dac23a5c10cb8e201b844839388d0c7d57
kkevi <77606317+kkevi@users.noreply.github.com>
2021-05-27 11:01:14 +0900

    Middle

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -17,8 +17,8 @@ function Profile(props) {
                     <Menu />
                     <Switch>
                         <Route exact path="/profile/:user/" component={My} />
-                        <Route exact path="/profile/:user/like" component={Like} />
-                        <Route exact path="/profile/:user/comment" component={Comment} />
+                        <Route exact path="/profile/like/:user" component={Like} />
+                        <Route exact path="/profile/comment/:user" component={Comment} />
                     </Switch>
                 </div>
             </div>
