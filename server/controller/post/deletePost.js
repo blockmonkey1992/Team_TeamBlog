2ce81ddc281bafed58919142ffa7ab47194155b4
허민서 <minseo@heominseoui-MacBookAir.local>
2021-06-01 16:29:18 +0900

    comment delete

--- server/controller/post/deletePost.js
+++ server/controller/post/deletePost.js
@@ -1,10 +1,11 @@
 const { Post } = require("../../model/Post");
-
+const { Comment} = require("../../model/Comment");
 
 const deletePost = async(req, res) => {
     const id = req.params.id
-    try {const result = await Post.findOneAndRemove({ _id: id})
-    res.status(200).json({ success : true , result});
+    try { await Post.findOneAndDelete({ _id: id})
+           Comment.deleteMany({ "postId" : id })         
+    res.status(200).json({ success : true });
     }catch(err) {
         throw err}};
 
