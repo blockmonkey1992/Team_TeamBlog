39b299c76f783301a716c12b94189612b35219ae
허민서 <minseo@heominseoui-MacBookAir.local>
2021-06-01 16:43:56 +0900

    like delete

--- server/controller/post/deletePost.js
+++ server/controller/post/deletePost.js
@@ -1,10 +1,13 @@
 const { Post } = require("../../model/Post");
 const { Comment} = require("../../model/Comment");
+const { Like } = require("../../model/Like");
+
 
 const deletePost = async(req, res) => {
     const id = req.params.id
     try { await Post.findOneAndDelete({ _id: id})
-           Comment.deleteMany({ "postId" : id })         
+           Comment.deleteMany({ "postId" : id })
+            Like.deleteMany({ "whichPost" : id})
     res.status(200).json({ success : true });
     }catch(err) {
         throw err}};
