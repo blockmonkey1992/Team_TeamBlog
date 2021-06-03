a228c34d192cd19bbb7f7a7dfa0bf0bc74f3cba3
허민서 <minseo@heominseoui-MacBookAir.local>
2021-06-03 11:34:48 +0900

    deleteMany.....

--- server/controller/post/deletePost.js
+++ server/controller/post/deletePost.js
@@ -3,13 +3,45 @@ const { Comment} = require("../../model/Comment");
 const { Like } = require("../../model/Like");
 
 
-const deletePost = async(req, res) => {
-    const id = req.params.id
-    try { await Post.findOneAndDelete({ _id: id})
-           Comment.deleteMany({ "postId" : id })
-            Like.deleteMany({ "whichPost" : id})
-    res.status(200).json({ success : true });
-    }catch(err) {
-        throw err}};
+const deletePost = (req, res) => {
+    const postId = req.params.id
+    Comment.deleteMany({ "postId" : postId }).exec((err, result) => {
+        if(err) {
+            throw err
+        } else { 
+            Like.deleteMany({ "whichPost" : postId}).exec((err, result) => {
+                if(err) {
+                    throw err
+                } else {
+                    Post.findOneAndDelete({ _id : postId}).exec((err, result) =>{
+                        if(err){
+                            throw err
+                        } else {
+                            res.status(200).json({ success : true });
+                        };
+                    });
+                };
+            });
+        };
+    });
+}
+        // Comment.findOneAndDelete({ "postId" : postId }).exec((err, result) => {
+        // if(err) { throw err 
+        // } 
+        // else {
+        //     Like.findOneAndDelete({ "whichPost" : postId }).exec((err, result) => {
+        //         if(err) {
+        //             throw err
+        //         } else {
+        //             Post.findOneAndDelete({ _id: postId}).exec((err, result) => {
+        //                 if(err) {
+        //                     throw err 
+        //                 }else {
+        //                     res.status(200).json({ success : true });
+                        
+                    
+
+            
+        
 
 module.exports = deletePost;
